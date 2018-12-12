1ee2b0c441a36996d668d7e2cf07ca1ba07e6f03
Kirk Jackson <kirk@kirkjackson.me.uk>
2018-12-12 11:43:36 +0000

    Avoid address update API bug that occurs when master_id is supplied but contact_id is not

--- CRM/API/Address.php
+++ CRM/API/Address.php
@@ -5,6 +5,15 @@ use CRM_API_ExtensionUtil as E;
 class CRM_API_Address extends CRM_API_ExtendableEntity {
 	protected static $properties;
 	
+	public function update($params, $always = TRUE) {
+		// A bug in CiviCRM causes a fatal error if master_id is supplied but contact_id is not.
+		if (!empty($params['master_id']) && !array_key_exists('contact_id', $params) && isset($this->contact_id)) {
+			$params['contact_id'] = $this->contact_id;
+		}
+		
+		parent::update($params, $always);
+	}
+	
 	public static function create($params, $cache = NULL) {
 		// If this address is shared with another contact, then copy address fields from the master address.
 		if (!empty($params['master_id'])) {
@@ -24,6 +33,7 @@ class CRM_API_Address extends CRM_API_ExtendableEntity {
 			'displayFields' => array('contact_id', 'street_address', 'city', 'postal_code', 'country_id'),
 			'fieldsMayNotMatchApiParams' => array('postal_code', 'geo_code_1', 'geo_code_2')
 		));
+		unset(static::$properties->persistFields['master_id']);
 		static::addParentRelationship('Contact', 'Address', 'Addresses', 'CRM_API_Contact', 'contact_id');
 	}
 }
